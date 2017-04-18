b221e6529f72f7d77cac9527a0a5315176dced70
Justin <just_255@yahoo.com>
2017-04-17 21:39:39 -0700

    Week 2 HW

--- app/src/main/java/com/justinsimmons/myapplication/MainActivity.java
+++ app/src/main/java/com/justinsimmons/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.justinsimmons.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickButton = (Button) findViewById(R.id.btnStartMessageActivity);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
