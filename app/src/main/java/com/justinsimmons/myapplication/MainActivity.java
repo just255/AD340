3ed557dc05060a2a29a1143501cfb380ec99ef51
Justin <just_255@yahoo.com>
2017-06-01 19:22:52 -0700

    location

--- app/src/main/java/com/justinsimmons/myapplication/MainActivity.java
+++ app/src/main/java/com/justinsimmons/myapplication/MainActivity.java
@@ -149,6 +149,10 @@ public class MainActivity extends AppCompatActivity {
                 intent = new Intent(MainActivity.this, RecyclerActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.map_menu:
+                intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+                break;
             default:
                 return super.onOptionsItemSelected(item);
         }
